411153aca716260ee8eb7e83a35f2b8012a3bb2d
ruqayaah <49168086+ruqayaahh@users.noreply.github.com>
2021-05-04 15:14:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 // eslint-disable-next-line import/extensions
 import userRouter from './routes/user.js';
 
@@ -11,14 +12,19 @@ dotenv.config();
 
 const index = express();
 
+index.use(cors);
+index.use(morgan('dev'));
 index.use(userRouter);
+
+index.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello' });
+});
 
 // index.use((req, res) => res.status(404).json({
 //   status: 'Fail',
 //   message: 'Not Found',
 // }));
 
-index.use(morgan('dev'));
 const dbName = `mongodb+srv://ruqayaah:${process.env.MONGO_PASSWORD}@cluster0.2lwgs.mongodb.net/tester`;
 
 mongoose.connect(dbName,
@@ -27,9 +33,5 @@ mongoose.connect(dbName,
     useUnifiedTopology: true,
     useCreateIndex: true,
   }).then(() => console.log('Connected to DB')).catch((err) => console.log(err));
-
-index.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello' });
-});
 
 index.listen(PORT, () => (console.log(`Listening @ ${PORT} again`)));
