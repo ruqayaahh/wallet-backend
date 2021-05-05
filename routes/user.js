21d6738caae5ea1123ec35e9f0934e5dbd1cbb51
ruqayaah <49168086+ruqayaahh@users.noreply.github.com>
2021-05-05 10:25:22 +0100

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 // import authenticate from '../middlewares/auth';
 // import validator from '../middlewares/user';
 import { saveUser } from '../controllers/user';
 
-const parser = bodyParser.json();
+// const parser = bodyParser.json();
 // const urlbodyParser = bodyParser.urlencoded({ extends: false });
 
 const userRouter = Router();
 
-userRouter.post('/register', parser, saveUser);
+userRouter.post('/register', saveUser);
 
 export default userRouter;
