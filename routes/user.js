d09217a1c1b01791c65d7e041127f7787ca94ccb
ruqayaah <aderinolaruqayaah@gmail.com>
2021-05-05 10:55:47 +0100

    i have come again

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,14 @@
 import { Router } from 'express';
-// import bodyParser from 'body-parser';
+import bodyParser from 'body-parser';
 // import authenticate from '../middlewares/auth';
 // import validator from '../middlewares/user';
 import { saveUser } from '../controllers/user';
 
-// const parser = bodyParser.json();
+const parser = bodyParser.json();
 // const urlbodyParser = bodyParser.urlencoded({ extends: false });
 
 const userRouter = Router();
 
-userRouter.post('/register', saveUser);
+userRouter.post('/register', parser, saveUser);
 
 export default userRouter;
