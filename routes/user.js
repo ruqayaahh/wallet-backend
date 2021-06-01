611e495b39f66d80f49843326d07ba7b175b609b
ruqayaah <aderinolaruqayaah@gmail.com>
2021-06-01 21:51:19 +0100

    heroku

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import bodyParser from 'body-parser';
 // import authenticate from '../middlewares/auth';
-import { checkExistingUser, saveUser, verifyUserOtp } from '../middlewares/user';
 import {
-  verifyOtp, updateOtpOnTimeout, updateVerifyUser, makeTransaction,
+  checkExistingUser,
+  saveUser,
+  verifyUserOtp,
+} from '../middlewares/user';
+import {
+  verifyOtp,
+  updateOtpOnTimeout,
+  updateVerifyUser,
+  makeTransaction,
 } from '../controllers/user';
 
 const parser = bodyParser.json();
@@ -16,7 +23,7 @@ userRouter.post('/register', parser, checkExistingUser, saveUser, verifyOtp);
 userRouter.post('/verify', parser, verifyUserOtp, updateVerifyUser);
 userRouter.post('/resend', parser, verifyOtp);
 userRouter.post('/otp_reset', parser, updateOtpOnTimeout);
-userRouter.post('/login', parser, checkIfUserExists, checkLoginDetails);
+// userRouter.post('/login', parser, checkIfUserExists, checkLoginDetails);
 userRouter.post('/transaction', parser, makeTransaction);
 
 export default userRouter;
